7a52ab0c552b504ef4d6dace168fd20e859a54ed
Cristian Castaño <cristiancastano852@gmail.com>
2023-09-11 12:33:05 -0500

    update cases component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from '../app/components/form/form.component';
 import { CasoComponent } from '../app/components/caso/caso.component';
 import { HomeComponent } from '../app/components/home/home.component';
+import { CasesComponent } from '../app/components/cases/cases.component';
+
 import { AuthGuard } from '../guards/auth.guard';
 const routes: Routes = [
   { path: 'crear-caso', component: FormComponent, canActivate: [AuthGuard] },
@@ -13,6 +15,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   { path: 'home', component: HomeComponent },
+  { path: 'mis-casos', component: CasesComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta predeterminada
 ];
 
--- src/app/components/caso/caso.component.ts
+++ src/app/components/caso/caso.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Case, CaseStatus, CaseType } from '../../modelos/case';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-caso',
@@ -14,13 +13,10 @@ export class CasoComponent {
 
   buscarCaso() {
     if (this.ticket) {
-      // Realizar la solicitud GET a la API
       this.http
         .get(`http://localhost:4000/cases/ticket/${this.ticket}`)
         .subscribe((data: any) => {
-          // Al recibir la respuesta, asignarla a la variable casoEncontrado
           this.casoEncontrado = data.case_;
-          console.log('Caso encontrado:', this.casoEncontrado);
         });
     }
   }
